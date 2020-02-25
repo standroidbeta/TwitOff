d31f04182cd6f3693fce780f8c762e38cd41d270
Stanley Kusmier <43893890+standroidbeta@users.noreply.github.com>
2020-02-25 13:49:49 -0500

    commented out DB code from predict.py

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -1,5 +1,5 @@
 """Prediction of user based on tweet embeddings"""
-
+import pickle
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from .models import User, Comparison, DB
@@ -20,15 +20,15 @@ def predict_user(user1_name, user2_name, tweet_text):
                              np.zeros(len(user2.tweets))])
     log_reg = LogisticRegression(solver='lbfgs', max_iter=1000).fit(embeddings, labels)
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
-    prediction = log_reg.predict(np.array(tweet_embedding).reshape(1, -1))[0]
-    predicted_user = user1_name if prediction == 1 else user2_name
-    pred_proba = log_reg.predict_proba(
-        np.array(tweet_embedding).reshape(1, -1))[0]
-    user1_prob = pred_proba[0]
-    user2_prob = pred_proba[1]
-    db_comparison = Comparison(text=tweet_text, predicted_user=predicted_user,
-                               user1_name=user1_name, user2_name=user2_name,
-                               user1_prob=user1_prob, user2_prob=user2_prob)
-    DB.session.add(db_comparison)
-    DB.session.commit()
-    return db_comparison
+    # prediction = log_reg.predict(np.array(tweet_embedding).reshape(1, -1))[0]
+    # predicted_user = user1_name if prediction == 1 else user2_name
+    # pred_proba = log_reg.predict_proba(
+    #     np.array(tweet_embedding).reshape(1, -1))[0]
+    # user1_prob = pred_proba[0]
+    # user2_prob = pred_proba[1]
+    # db_comparison = Comparison(text=tweet_text, predicted_user=predicted_user,
+    #                            user1_name=user1_name, user2_name=user2_name,
+    #                            user1_prob=user1_prob, user2_prob=user2_prob)
+    # DB.session.add(db_comparison)
+    # DB.session.commit()
+    return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
